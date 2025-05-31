a6ecaaf699b906584d364816206f7557fe484fca
JefferPistala <jefferson.pistala@epn.edu.ec>
2025-05-31 09:34:18 -0500

    cambio de variable

--- src/models/posturas.php
+++ src/models/posturas.php
@@ -18,7 +18,7 @@ $stmt = $pdo->query($sql);
   <div class="contenedor-posturas">
     <?php foreach ($stmt as $fila): ?>
       <div class="postura">
-        <img src="<?php echo $fila['imagen_url']; ?>" alt="Postura">
+        <img src="<?php echo $fila['foto_url']; ?>" alt="Postura">
         <h3><?php echo $fila['nombre_es']; ?></h3>
         <p><strong>Beneficios:</strong> <?php echo $fila['beneficios']; ?></p>
         <p><strong>Instrucciones:</strong> <?php echo $fila['instrucciones']; ?></p>
